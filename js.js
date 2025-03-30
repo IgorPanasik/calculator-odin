dfc87de231d41faef9f4d63e66ab2554e25195fc
IgorPanasik <102683696+IgorPanasik@users.noreply.github.com>
2025-03-30 21:25:11 +0300

    feat(calculator): Add other functions operandZero() addDecimal(), modifyNumber(), allClear()
    - Add operandZero() allows correct handling of leading 0.
    - Add addDecimal() allows correct handling of decimal
    values.
    - Add modifyNumber() allows you to correctly calculate %
    and change the +/- sign.
    - Add allClear() Allows you to clear all current
    operations and values.
    
    The main tasks of the project calculator for course
    Odin are completed.

--- js.js
+++ js.js
@@ -5,106 +5,170 @@ const operators = document.querySelectorAll('.operator');
 let operandOne = '';
 let operator = null;
 let operandTwo = '';
-let result = 0;
+let result = null;
+let isPercentage = false;
 
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
-const divide = (a, b) => (a !== 0 ? a / b : 'Error');
+const divide = (a, b) => {
+	if (b === 0) {
+		display.textContent = 'lmao';
+		operandOne = '';
+		operandTwo = '';
+		operator = null;
+		result = null;
+		return NaN;
+	}
+	return a / b;
+};
 
-const operate = (operandOne, operator, operandTwo) => {
+const operate = (a, operator, b) => {
+	let result;
 	switch (operator) {
 		case '+':
-			return add(operandOne, operandTwo);
+			result = add(a, b);
+			break;
 		case '-':
-			return subtract(operandOne, operandTwo);
+			result = subtract(a, b);
+			break;
 		case '*':
-			return multiply(operandOne, operandTwo);
+			result = multiply(a, b);
+			break;
 		case '/':
-			return divide(operandOne, operandTwo);
+			result = divide(a, b);
+			break;
 		default:
 			return display.textContent;
 	}
+	return parseFloat(result.toFixed(10));
 };
 
-const resetActiveOperators = () => {
+// Function to clear active operators
+const resetActiveOperators = () =>
 	operators.forEach((op) => op.classList.remove('active'));
-};
 
+//  Visual effect of button blinking
 const addEffectBlink = (button) => {
 	if (!button.classList.contains('operators')) {
 		button.classList.add('blink');
-		setTimeout(() => button.classList.remove('blink'), 200);
+		setTimeout(() => button.classList.remove('blink'), 100);
 	}
 };
 
 const clickOperands = (e) => {
-	if (e.target.classList.contains('operand')) {
-		if (display.textContent.length < 18) {
-			resetActiveOperators();
-			addEffectBlink(e.target);
-			if (!operator) {
-				if (result !== null) {
-					operandOne = '';
-					result = null;
-				}
-				operandOne += e.target.textContent;
-				display.textContent = operandOne;
-			} else {
-				operandTwo += e.target.textContent;
-				display.textContent = operandTwo;
-			}
+	if (
+		e.target.classList.contains('operand') &&
+		display.textContent.length < 18
+	) {
+		resetActiveOperators();
+		addEffectBlink(e.target);
+		if (!operator) {
+			if (result !== null) (operandOne = ''), (result = null);
+			operandOne = operandZero(operandOne, e.target.textContent);
+			display.textContent = operandOne;
+		} else {
+			operandTwo = operandZero(operandTwo, e.target.textContent);
+			display.textContent = operandTwo;
 		}
 	}
 };
 
 const clickOperator = (e) => {
-	if (e.target.classList.contains('operator')) {
-		if (operandOne && operator && operandTwo) {
-			result = operate(
-				parseFloat(operandOne),
-				operator,
-				parseFloat(operandTwo),
-			);
-			operandOne = result;
-			operandTwo = '';
-			display.textContent = result;
-		} else if (operandOne && operator && !operandTwo) {
-			result = operate(
-				parseFloat(operandOne),
-				operator,
-				parseFloat(operandOne),
-			);
-			operandOne = result;
-			display.textContent = result;
-		}
+	if (!e.target.classList.contains('operator')) return;
+	if (isPercentage) isPercentage = false;
+
+	if (operator && !operandTwo) {
+		operator = e.target.textContent;
 		resetActiveOperators();
 		e.target.classList.add('active');
-		operator = e.target.textContent;
-		operandTwo = '';
+		return;
 	}
+
+	if (operandOne && operandTwo) {
+		result = operate(parseFloat(operandOne), operator, parseFloat(operandTwo));
+		operandOne = result;
+		operandTwo = '';
+		display.textContent = result;
+	}
+
+	resetActiveOperators();
+	e.target.classList.add('active');
+	operator = e.target.textContent;
 };
 
 const clickEquals = (e) => {
-	if (e.target.classList.contains('equals')) {
-		resetActiveOperators();
-		addEffectBlink(e.target);
-		if (operandOne && operator) {
-			result = operandTwo
-				? operate(parseFloat(operandOne), operator, parseFloat(operandTwo))
-				: operate(parseFloat(operandOne), operator, parseFloat(operandOne));
-			operandOne = result;
-			operandTwo = '';
-			operator = null;
-			display.textContent = result;
-		}
+	if (!e.target.classList.contains('equals')) return;
+	resetActiveOperators();
+	addEffectBlink(e.target);
+
+	if (operandOne && operator) {
+		result = operate(
+			parseFloat(operandOne),
+			operator,
+			parseFloat(operandTwo || operandOne),
+		);
+
+		if (isNaN(result)) return;
+		operandOne = result;
+		operandTwo = '';
+		operator = null;
+		display.textContent = result;
 	}
 };
 
+// Function for handling leading zeros in numbers
+const operandZero = (operand, input) =>
+	operand === '0' ? input : operand + input;
+
+const addDecimal = (e) => {
+	if (e.target.textContent !== '.') return;
+	addEffectBlink(e.target);
+
+	if (!operator) {
+		if (!operandOne.includes('.')) operandOne += operandOne ? '.' : '0.';
+		display.textContent = operandOne;
+	} else {
+		if (!operandTwo.includes('.')) operandTwo += operandTwo ? '.' : '0.';
+		display.textContent = operandTwo;
+	}
+};
+
+// Changing sign and percentage
+const modifyNumber = (e) => {
+	if (
+		!e.target.classList.contains('sign') &&
+		!e.target.classList.contains('percent')
+	)
+		return;
+	let modifier = e.target.classList.contains('sign') ? -1 : 0.01;
+
+	if (!operator) {
+		operandOne = (parseFloat(operandOne) * modifier).toString();
+		display.textContent = operandOne;
+	} else {
+		operandTwo = (parseFloat(operandTwo) * modifier).toString();
+		display.textContent = operandTwo;
+	}
+
+	if (e.target.classList.contains('percent')) isPercentage = true;
+};
+
+const allClear = (e) => {
+	if (!e.target.classList.contains('clear')) return;
+	operandOne = operandTwo = '';
+	operator = result = null;
+	display.textContent = '0';
+};
+
+// Function to refresh the display on click
 const updateDisplay = (e) => {
 	clickOperands(e);
 	clickOperator(e);
 	clickEquals(e);
+	addDecimal(e);
+	modifyNumber(e);
+	allClear(e);
 };
 
 buttons.addEventListener('click', updateDisplay);
